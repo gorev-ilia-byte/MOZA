aed0027de15129e4977f374dc651df80f87a01b5
gorev-ilia-byte <gorev-ilia@mail.ru>
2026-04-02 17:49:12 +0300

    Delete app/applet directory

--- app/applet/debug.js
+++
@@ -1,4 +0,0 @@
-import Database from 'better-sqlite3';
-const db = new Database('messenger.db');
-const schema = db.prepare("PRAGMA table_info(messages)").all();
-console.log(JSON.stringify(schema, null, 2));
